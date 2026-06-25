76e315983455dcabd25acb8fe3daa10c808ebc54
Mohd Faiz <mohdfaiz1472002@gmail.com>
2026-06-25 15:05:40 +0530

    Initial commit

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -483,7 +483,7 @@ export default function Header() {
 
           <nav className="hidden md:flex items-center gap-8 ml-auto">
             <Link to="/" className="header-nav-link">
-              Home
+              Homepro
             </Link>
 
             <div className="static" ref={menuRef}>
